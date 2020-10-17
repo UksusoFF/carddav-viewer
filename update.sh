c30d5b7f1920bc99c1ff1173c785d20ca162bba5
Kirill Uksusov <uksusoff@yandex.ru>
2020-10-17 13:18:58 +0400

    Updater updated

--- update.sh
+++ update.sh
@@ -3,4 +3,6 @@
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 UPDATE_URL=https://codeload.github.com/UksusoFF/carddav-viewer/tar.gz/master
 
-wget -qO- "${UPDATE_URL}" | tar --transform 's/^dbt2-0.37.50.3/dbt2/' -xvz -C "${SCRIPT_DIR}" --strip 1 --exclude=update.sh
+wget -qO- "${UPDATE_URL}" --no-cache | tar --transform 's/^dbt2-0.37.50.3/dbt2/' -xvz -C "${SCRIPT_DIR}" --strip 1 --exclude=update.sh
+
+composer install
